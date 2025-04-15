4eeaa6015ca5bef42e5aa1c3ad752924f8a8efa8
AlejandroLunaDev <alejandrolunadev@gmail.com>
2025-04-15 11:16:52 -0300

    Add drug-free messaging to English and Spanish locale files

--- frontend/platform/shared/layout/sidebar/components/Logo.jsx
+++ frontend/platform/shared/layout/sidebar/components/Logo.jsx
@@ -1,17 +1,22 @@
 'use client';
 
+import { Link } from '@/i18n/routing';
+
 /**
  * Componente que renderiza el logo de la aplicación
+ * Al hacer clic en él, navega a la página raíz
  */
 const Logo = () => {
   return (
-    <div className='flex items-center justify-center bg-white rounded-lg p-2'>
-      <img
-        src='https://framerusercontent.com/images/jhXEwk0PeDpLgoJ5PZFagA5OpQ.svg'
-        alt='Play Attention Logo'
-        className='h-8'
-      />
-    </div>
+    <Link href="/" className='block'>
+      <div className='flex items-center justify-center bg-white rounded-lg p-2 cursor-pointer hover:opacity-90 transition-opacity'>
+        <img
+          src='https://framerusercontent.com/images/jhXEwk0PeDpLgoJ5PZFagA5OpQ.svg'
+          alt='Play Attention Logo'
+          className='h-8'
+        />
+      </div>
+    </Link>
   );
 };
 
